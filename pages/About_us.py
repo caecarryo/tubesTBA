c8b3c0d1e468942d8fd3a5aded0db866902f8f8d
caecarryo <64115111+caecarryo@users.noreply.github.com>
2023-06-18 19:29:29 +0700

    Create About_us.py

---
+++ pages/About_us.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("About Us")
+
+st.title("Kelompok 05")
+st.write("Caecarryo Bagus Dewanata - 1301213514")
+st.write("Wahyu Nata Mahendra - 1301213101")
+st.write("Sintia Dwi Cahya - 1301213440")
